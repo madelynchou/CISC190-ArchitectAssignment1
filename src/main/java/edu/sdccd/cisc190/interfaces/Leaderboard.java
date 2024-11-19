769fb29ad32490ac332808e51f573d18fa230961
Julian Garcia <69781917+julianalg@users.noreply.github.com>
2024-11-19 13:33:45 -0800

    removing unused imports

--- src/main/java/edu/sdccd/cisc190/interfaces/Leaderboard.java
+++ src/main/java/edu/sdccd/cisc190/interfaces/Leaderboard.java
@@ -2,17 +2,7 @@ package edu.sdccd.cisc190.interfaces;
 
 import edu.sdccd.cisc190.players.bots.*;
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class Leaderboard extends Application {
@@ -25,7 +15,7 @@ public class Leaderboard extends Application {
     }
 
     public static void showWindow(Stage primaryStage) {
-        
+
     }
 //        primaryStage.setTitle("Casino Royale - Leaderboard");
 //
