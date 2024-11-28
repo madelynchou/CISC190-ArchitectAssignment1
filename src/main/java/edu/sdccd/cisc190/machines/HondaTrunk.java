b9679890f473c2624e68215c58b26012e5e64827
jaydenb2008 <jjeromebui2008@gmail.com>
2024-11-28 15:30:40 -0800

    update slot payout

--- src/main/java/edu/sdccd/cisc190/machines/HondaTrunk.java
+++ src/main/java/edu/sdccd/cisc190/machines/HondaTrunk.java
@@ -5,13 +5,26 @@ public class HondaTrunk extends Slot {
         super(new String[]{"🚗", "🛻", "🚕"}, 1000, 1, 1.5);
     }
 
-    //the user will only lose have the bet they place
+    //honda trunk will have one match wins a tenth of full match
+    @Override
+    public int evaluateWinCondition(String[] arr) {
+        if (arr[0].equals(arr[1]) && arr[1].equals(arr[2])) {
+            return 3; // Full match
+        } else if (arr[0].equals(arr[1]) || arr[1].equals(arr[2]) || arr[0].equals(arr[2])) {
+            return 2;
+        } else {
+            return 0;
+        }
+    }
+
     @Override
     public int calculatePayout(int moneyAmount, String[] spunRow, int bet) {
         int winningCondition = evaluateWinCondition(spunRow);
         return switch (winningCondition) {
             case 0 -> // No match
-                    (int) (moneyAmount - (Math.floor(bet * 0.5)));
+                    moneyAmount - bet;
+            case 2 ->
+                    (int) (moneyAmount + Math.floor(bet * returnAmt * 0.1));
             case 3 -> // Three-symbol match
                     (int) (moneyAmount + Math.floor(bet * returnAmt));
             default -> moneyAmount;
