4a713902402ba323ba7772ae3308d46e55d893e7
jaydenb2008 <jjeromebui2008@gmail.com>
2024-11-28 15:30:40 -0800

    update slot payout

--- src/main/java/edu/sdccd/cisc190/machines/DiamondDash.java
+++ src/main/java/edu/sdccd/cisc190/machines/DiamondDash.java
@@ -5,26 +5,13 @@ public class DiamondDash extends Slot {
         super(new String[]{"💍", "💠", "💎"}, 1000, 15, 2);
     }
 
-    //diamond dash will have one match wins half of full match
-    @Override
-    public int evaluateWinCondition(String[] arr) {
-        if (arr[0].equals(arr[1]) && arr[1].equals(arr[2])) {
-            return 3; // Full match
-        } else if (arr[0].equals(arr[1]) || arr[1].equals(arr[2]) || arr[0].equals(arr[2])) {
-            return 2;
-        } else {
-            return 0;
-        }
-    }
-
+    //the user will only lose have the bet they place
     @Override
     public int calculatePayout(int moneyAmount, String[] spunRow, int bet) {
         int winningCondition = evaluateWinCondition(spunRow);
         return switch (winningCondition) {
             case 0 -> // No match
-                    moneyAmount - bet;
-            case 2 ->
-                    (int) (moneyAmount + Math.floor(bet * returnAmt * 0.5));
+                    (int) (moneyAmount - (Math.floor(bet * 0.5)));
             case 3 -> // Three-symbol match
                     (int) (moneyAmount + Math.floor(bet * returnAmt));
             default -> moneyAmount;
