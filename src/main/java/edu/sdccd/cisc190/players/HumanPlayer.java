41959331bfe343f7a6aa1aa608428420a9a6195a
madelynchou <madelyn7chou@gmail.com>
2024-12-07 23:28:41 -0800

    Update HumanPlayer.java

--- src/main/java/edu/sdccd/cisc190/players/HumanPlayer.java
+++ src/main/java/edu/sdccd/cisc190/players/HumanPlayer.java
@@ -16,6 +16,7 @@ public class HumanPlayer {
     private HumanPlayer() {}
 
     public static HumanPlayer getInstance() {
+        // TODO: Chek if using singleton pattern is necessary for this class
 
         if (instance == null) {
             instance = new HumanPlayer();
@@ -24,10 +25,12 @@ public class HumanPlayer {
     }
 
     public void setUsername(String username) {
+        // TODO: Add validation to ensure username is not null or empty
         this.username = username;
     }
 
     public final void setMoney(int value) {
+        // TODO: Ensure money value cannot be negative
         money.set(value);
     }
 
@@ -36,10 +39,12 @@ public class HumanPlayer {
     }
 
     public IntegerProperty moneyProperty() {
+        // TODO: Document why money is an IntegerProperty (used in JavaFX bindings)
         return money;
     }
 
     public String getName() {
+        // TODO: Consider renaming this method to "getUsername" for consistency
         return username;
     }
 }
