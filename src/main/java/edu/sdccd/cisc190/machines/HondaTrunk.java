75b10e251670ada996f653f98c43cb2bddf3f03b
madelynchou <madelyn7chou@gmail.com>
2024-12-09 00:40:41 -0800

    Update HondaTrunk.java
    The comments I added are meant to make the code easier to follow. I suggested adding explanations about what the parameters in the super constructor mean and how the evaluateWinCondition method works, especially how it decides if there are 3 matches, 2 matches, or no matches. I also pointed out the need to explain why partial matches result in a smaller payout and how the calculation for that works. I asked for more details on why the bet amount is subtracted when there's no match and why the full match payout uses the returnAmt. Finally, I recommended clarifying the behavior of the default case in the payout method. These comments aim to help anyone reading the code understand why and how things are calculated.

--- src/main/java/edu/sdccd/cisc190/machines/HondaTrunk.java
+++ src/main/java/edu/sdccd/cisc190/machines/HondaTrunk.java
@@ -8,6 +8,7 @@ package edu.sdccd.cisc190.machines;
  */
 public class HondaTrunk extends Slot {
     public HondaTrunk() {
+        // TODO: Add comments explaining what each parameter in the super constructor represents
         super(new String[]{"🚗", "🛻", "🚕"}, 1000, 1, 1.5);
     }
 
@@ -19,6 +20,7 @@ public class HondaTrunk extends Slot {
      */
     @Override
     public int evaluateWinCondition(String[] arr) {
+        // TODO: Clarify the logic behind checking if two or three symbols match and return the correct value
         if (arr[0].equals(arr[1]) && arr[1].equals(arr[2])) {
             return 3; // Full match
         } else if (arr[0].equals(arr[1]) || arr[1].equals(arr[2]) || arr[0].equals(arr[2])) {
@@ -38,15 +40,19 @@ public class HondaTrunk extends Slot {
      */
     @Override
     public int calculatePayout(int moneyAmount, String[] spunRow, int bet) {
+         // TODO: Explain why partial matches give a smaller payout and how it's calculated
         int winningCondition = evaluateWinCondition(spunRow);
         return switch (winningCondition) {
             case 0 -> // No match
+                // TODO: Add reasoning behind deducting the bet amount in case of no match
                     moneyAmount - bet;
             case 2 ->
+                // TODO: Clarify the purpose of multiplying by 0.25 for partial match payout
                     (int) (moneyAmount + Math.floor(bet * returnAmt * 0.25));
             case 3 -> // Three-symbol match
                     (int) (moneyAmount + Math.floor(bet * returnAmt));
             default -> moneyAmount;
+                // TODO: Explain why default may return the original moneyAmount
         };
     }
 
